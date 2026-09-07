868cf478169b151977df8f11aa47d3d6159f9902
vaniasouza-hub <vania.souza.estudos@gmail.com>
2026-09-07 00:05:09 -0300

    primeiro_projeto

---
+++ README.md
@@ -0,0 +1,8 @@
+print(f"\nAluno: {nome}")
+print(f"Media: {media:.2f}")
+
+if media >= 6:
+    print("Situacao: Aprovado")
+else:
+    print("Situacao: Reprovado")
+
---
+++ app.py
@@ -0,0 +1,7 @@
+# Programa de cálculo de média de notasAutora: Maria Vania
+# Entrada
+nome = input("Digite o nome do aluno: ")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
